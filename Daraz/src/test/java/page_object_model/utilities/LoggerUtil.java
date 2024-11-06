9de99ae1488c1091911e216df37ec71645288b2a
saumya-699 <92803599+saumya-699@users.noreply.github.com>
2024-11-07 00:07:28 +0530

    Added LoggerUtil class

---
+++ Daraz/src/test/java/page_object_model/utilities/LoggerUtil.java
@@ -0,0 +1,17 @@
+package page_object_model.utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerUtil {
+    private static final Logger logger = LogManager.getLogger(LoggerUtil.class);
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+}
+
